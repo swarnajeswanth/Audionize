7d48833d83ee2a1d3fe3ffa1d9d04a826a05ea81
swarnaejeswanth <swarnajeswanth1234@gmail.com>
2025-07-11 15:45:01 +0530

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -50,7 +50,7 @@ export function middleware(request) {
   // Update Content Security Policy for WebSocket connections and allow inline styles/scripts
   response.headers.set(
     "Content-Security-Policy",
-    "default-src 'self'; connect-src 'self' ws: wss: wss://aduionize-socket.onrender.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline';"
+    "default-src 'self'; connect-src 'self' ws: wss: https://aduionize-socket.onrender.com wss://aduionize-socket.onrender.com; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline';"
   );
 
   return response;
